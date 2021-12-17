da00c82a3f7a7db32db889114cd8a23633c6716e
Ryan Grimes <ryanpaulgrimes@gmail.com>
2021-12-17 12:26:40 +0100

    Redundant tests related to room and messages removed

--- spec/requests/messages_spec.rb
+++ spec/requests/messages_spec.rb
@@ -17,114 +17,114 @@ RSpec.describe "/messages", type: :request do
   # Message. As you add validations to Message, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    # skip("Add a hash of attributes valid for your model")
   }
 
   let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
+    # skip("Add a hash of attributes invalid for your model")
   }
 
   describe "GET /index" do
-    it "renders a successful response" do
-      Message.create! valid_attributes
-      get messages_url
-      expect(response).to be_successful
-    end
+    # it "renders a successful response" do
+    #   Message.create! valid_attributes
+    #   get messages_url
+    #   expect(response).to be_successful
+    # end
   end
 
   describe "GET /show" do
-    it "renders a successful response" do
-      message = Message.create! valid_attributes
-      get message_url(message)
-      expect(response).to be_successful
-    end
+    # it "renders a successful response" do
+    #   message = Message.create! valid_attributes
+    #   get message_url(message)
+    #   expect(response).to be_successful
+    # end
   end
 
   describe "GET /new" do
-    it "renders a successful response" do
-      get new_message_url
-      expect(response).to be_successful
-    end
+    # it "renders a successful response" do
+    #   get new_message_url
+    #   expect(response).to be_successful
+    # end
   end
 
   describe "GET /edit" do
-    it "render a successful response" do
-      message = Message.create! valid_attributes
-      get edit_message_url(message)
-      expect(response).to be_successful
-    end
+    # it "render a successful response" do
+    #   message = Message.create! valid_attributes
+    #   get edit_message_url(message)
+    #   expect(response).to be_successful
+    # end
   end
 
   describe "POST /create" do
     context "with valid parameters" do
-      it "creates a new Message" do
-        expect {
-          post messages_url, params: { message: valid_attributes }
-        }.to change(Message, :count).by(1)
-      end
+      # it "creates a new Message" do
+      #   expect {
+      #     post messages_url, params: { message: valid_attributes }
+      #   }.to change(Message, :count).by(1)
+      # end
 
-      it "redirects to the created message" do
-        post messages_url, params: { message: valid_attributes }
-        expect(response).to redirect_to(message_url(Message.last))
-      end
+      # it "redirects to the created message" do
+      #   post messages_url, params: { message: valid_attributes }
+      #   expect(response).to redirect_to(message_url(Message.last))
+      # end
     end
 
     context "with invalid parameters" do
-      it "does not create a new Message" do
-        expect {
-          post messages_url, params: { message: invalid_attributes }
-        }.to change(Message, :count).by(0)
-      end
+      # it "does not create a new Message" do
+      #   expect {
+      #     post messages_url, params: { message: invalid_attributes }
+      #   }.to change(Message, :count).by(0)
+      # end
 
-      it "renders a successful response (i.e. to display the 'new' template)" do
-        post messages_url, params: { message: invalid_attributes }
-        expect(response).to be_successful
-      end
+      # it "renders a successful response (i.e. to display the 'new' template)" do
+      #   post messages_url, params: { message: invalid_attributes }
+      #   expect(response).to be_successful
+      # end
     end
   end
 
   describe "PATCH /update" do
     context "with valid parameters" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
+      # let(:new_attributes) {
+      #   skip("Add a hash of attributes valid for your model")
+      # }
 
-      it "updates the requested message" do
-        message = Message.create! valid_attributes
-        patch message_url(message), params: { message: new_attributes }
-        message.reload
-        skip("Add assertions for updated state")
-      end
+      # it "updates the requested message" do
+      #   message = Message.create! valid_attributes
+      #   patch message_url(message), params: { message: new_attributes }
+      #   message.reload
+      #   skip("Add assertions for updated state")
+      # end
 
-      it "redirects to the message" do
-        message = Message.create! valid_attributes
-        patch message_url(message), params: { message: new_attributes }
-        message.reload
-        expect(response).to redirect_to(message_url(message))
-      end
+      # it "redirects to the message" do
+      #   message = Message.create! valid_attributes
+      #   patch message_url(message), params: { message: new_attributes }
+      #   message.reload
+      #   expect(response).to redirect_to(message_url(message))
+      # end
     end
 
-    context "with invalid parameters" do
-      it "renders a successful response (i.e. to display the 'edit' template)" do
-        message = Message.create! valid_attributes
-        patch message_url(message), params: { message: invalid_attributes }
-        expect(response).to be_successful
-      end
-    end
+    # context "with invalid parameters" do
+    #   # it "renders a successful response (i.e. to display the 'edit' template)" do
+    #   #   message = Message.create! valid_attributes
+    #   #   patch message_url(message), params: { message: invalid_attributes }
+    #   #   expect(response).to be_successful
+    #   end
+    # end
   end
 
   describe "DELETE /destroy" do
-    it "destroys the requested message" do
-      message = Message.create! valid_attributes
-      expect {
-        delete message_url(message)
-      }.to change(Message, :count).by(-1)
-    end
+    # it "destroys the requested message" do
+    #   message = Message.create! valid_attributes
+    #   expect {
+    #     delete message_url(message)
+    #   }.to change(Message, :count).by(-1)
+    # end
 
-    it "redirects to the messages list" do
-      message = Message.create! valid_attributes
-      delete message_url(message)
-      expect(response).to redirect_to(messages_url)
-    end
+    # it "redirects to the messages list" do
+    #   message = Message.create! valid_attributes
+    #   delete message_url(message)
+    #   expect(response).to redirect_to(messages_url)
+    # end
   end
 end
